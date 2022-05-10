1ae0832a2b79d9910260020908c4a0f7ece916d9
gustavoquincy <yuankaile@gmail.com>
2022-05-10 11:47:54 +0800

    matching the request and handling requests to/differently from other requests

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,22 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
-    // println!("Received: {}", String::from_utf8_lossy(&buffer[..]));
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        // some other request
+    }
 }
